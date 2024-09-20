87599b8cf1ff42fdd927538bf05b8d5a444f0b4c
dvnguyen02 <duynguyen290502@gmail.com>
2024-09-20 14:50:10 +1200

    add codes to clear cache after each page

--- parser.py
+++ parser.py
@@ -1,61 +1,69 @@
 import pdfplumber
 import csv
-import re, json
+import re
 from collections import defaultdict
 
-pdf = pdfplumber.open("from1-09to10-09.pdf")
-
-with open('mttq.csv', 'w', newline='') as csvfile:
-    fieldnames = ['date', 'transaction_code', 'amount', 'transaction_detail',
-                    # 'test'
-                 ]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+def process_page(page):
+    transactions = []
+    date = None
+    start = False
+    info = defaultdict(str)
+    i = 0
     
-    for page in pdf.pages:
-        start = False
-        i = 0
-        info = defaultdict(str)
-        
-        transactions = []
-        date = None
-        
-        text_chunks = page.extract_text().split('\n')
-        for text_chunk in text_chunks:
-            if text_chunk == 'Postal address: Telex : (0805) 411504 VCB - VT':
-                break
-            if not start and text_chunk == 'Số CT/ Doc No':
-                start = True
-            elif start:
-                match = re.fullmatch('[0-9]{2}\/09\/2024', text_chunk)
-                if match:
-                    i = 0
-                    if info['transaction_code']:
-                        info['date'] = date
-                        # info['test'] = json.dumps(info['transaction_detail'].split('.'))
-                        transactions.append(info)
-                        info = defaultdict(str)
-                    date = match.group(0)
-                elif i == 0:
-                    amount, first_transaction_detail_line = text_chunk.split(' ', 1)
-                    info['amount'] = amount
-                    info['transaction_detail'] = first_transaction_detail_line
-                    match1 = re.match('(\d{1,3}(\.\d{3})*) ', first_transaction_detail_line)
-                    if match1:
-                        print(match1.group(1))
-                        info['transaction_detail'] = info['transaction_detail'].split(' ', 1)[1]
-                    i += 1
-                elif i == 1:
-                    info['transaction_code'] = text_chunk
-                    i += 1
-                else:
-                    info['transaction_detail'] += ' ' + text_chunk
+    text_chunks = page.extract_text().split('\n')
+    for text_chunk in text_chunks:
+        if text_chunk == 'Postal address: Telex : (0805) 411504 VCB - VT':
+            break
+        if not start and text_chunk == 'Số CT/ Doc No':
+            start = True
+        elif start:
+            match = re.fullmatch('[0-9]{2}\/09\/2024', text_chunk)
+            if match:
+                i = 0
+                if info['transaction_code']:
+                    info['date'] = date
+                    transactions.append(info)
+                    info = defaultdict(str)
+                date = match.group(0)
+            elif i == 0:
+                amount, first_transaction_detail_line = text_chunk.split(' ', 1)
+                info['amount'] = amount
+                info['transaction_detail'] = first_transaction_detail_line
+                match1 = re.match('(\d{1,3}(\.\d{3})*) ', first_transaction_detail_line)
+                if match1:
+                    print(match1.group(1))
+                    info['transaction_detail'] = info['transaction_detail'].split(' ', 1)[1]
+                i += 1
+            elif i == 1:
+                info['transaction_code'] = text_chunk
+                i += 1
+            else:
+                info['transaction_detail'] += ' ' + text_chunk
 
-        if info['transaction_code']:
-            info['date'] = date
-            # info['test'] = json.dumps(info['transaction_detail'].split('.'))
-            transactions.append(info)
+    if info['transaction_code']:
+        info['date'] = date
+        transactions.append(info)
+    
+    return transactions
 
-        # print(page, len(transactions))
-        for row in transactions:
-            writer.writerow(row)
+def main():
+    pdf_path = "from1-09to10-09.pdf"
+    
+    # Get the total number of pages
+    with pdfplumber.open(pdf_path) as pdf:
+        num_pages = len(pdf.pages)
+
+    with open('mttq.csv', 'w', newline='') as csvfile:
+        fieldnames = ['date', 'transaction_code', 'amount', 'transaction_detail']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for page_number in range(num_pages):
+            with pdfplumber.open(pdf_path) as pdf:
+                page = pdf.pages[page_number]
+                transactions = process_page(page)
+                for row in transactions:
+                    writer.writerow(row)
+
+if __name__ == "__main__":
+    main()
